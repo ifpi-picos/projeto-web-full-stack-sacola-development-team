769e2136babd6c5450c729010a75d13198c554e6
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2023-10-01 16:42:58 -0300

    ajustando design e responsividade de telas

--- src/components/Utils/CloudinaryUploadWidget.tsx
+++ src/components/Utils/CloudinaryUploadWidget.tsx
@@ -1,21 +1,17 @@
-import { CldUploadWidget } from 'next-cloudinary';
-import { useState } from 'react'; 
+import { CldUploadWidget } from "next-cloudinary";
+import { useState } from "react";
 
-interface UploadImageProps{
+interface UploadImageProps {
   onURLChange?: (url: string) => void;
-
-
 }
 
+const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
 
-const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET; 
-
-export default function CloudinaryUploadWidget( {onURLChange}: UploadImageProps) {
-  const [imageURL, setImageURL] = useState(''); // Inicializa o estado imageURL como uma string vazia
-
-
-
-
+export default function CloudinaryUploadWidget({
+  onURLChange,
+}: UploadImageProps) {
+  const [imageURL, setImageURL] = useState(""); // Inicializa o estado imageURL como uma string vazia
+  const [buttonText, setButtonText] = useState("Enviar Imagem");
   // Função chamada quando o upload é bem-sucedido
   function handleUploadSuccess(result: any) {
     // A URL da imagem pode ser acessada através da propriedade secure_url do resultado
@@ -23,29 +19,39 @@ export default function CloudinaryUploadWidget( {onURLChange}: UploadImageProps)
     if (onURLChange) {
       onURLChange(imageUrl);
     } else {
-      console.warn('Você precisa passar a prop onChange para o formulário de cadastro');  
+      console.warn(
+        "Você precisa passar a prop onChange para o formulário de cadastro"
+      );
     }
-    console.log('Upload bem-sucedido. URL da imagem:', imageUrl);
+    console.log("Upload bem-sucedido. URL da imagem:", imageUrl);
 
     // Atualiza o estado com a URL da imagem
     setImageURL(imageUrl);
+
+    setButtonText("Reenviar Imagem")
   }
 
   return (
     <div>
       {/* Componente CldUploadWidget que permite fazer upload de imagens */}
-      <CldUploadWidget uploadPreset={uploadPreset} onSuccess={handleUploadSuccess}>
+      <CldUploadWidget
+        uploadPreset={uploadPreset}
+        onSuccess={handleUploadSuccess}
+      >
         {({ open }) => {
           // Função chamada quando o botão "Upload an Image" é clicado
-          function handleOnClick(e: { preventDefault: () => void; }) {
+          function handleOnClick(e: { preventDefault: () => void }) {
             e.preventDefault();
             open();
           }
 
           return (
             // Botão que inicia o processo de upload quando clicado
-            <button className="button p-2 bg-blue-400" onClick={handleOnClick}>
-             Enviar imagem
+            <button
+              className="bg-blue-400 w-full  text-white py-2 px-4 rounded hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 active:bg-blue-600  "
+              onClick={handleOnClick}
+            >
+              Enviar Imagem
             </button>
           );
         }}
