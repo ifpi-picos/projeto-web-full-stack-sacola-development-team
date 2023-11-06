ec21d700b3b2a8347a4e3bfdb460b80394a2b006
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2023-11-06 16:44:14 -0300

    Adicionando componente de Loading na tela de biblioteca

--- src/components/Utils/CardModal.tsx
+++ src/components/Utils/CardModal.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import { useState } from "react";
-import IconButton from "@mui/material/IconButton/IconButton";
+import IconButton from "@mui/material/IconButton";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Menu from "@mui/material/Menu/Menu";
-import MenuItem from "@mui/material/MenuItem/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon/ListItemIcon";
-
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
 
 export default function CardModal() {
   const [open, setOpen] = useState(false);
@@ -23,9 +22,9 @@ export default function CardModal() {
   };
 
   return (
-    <div className='flex justify-end relative  top-10 sm:right-2'>
+    <div className="flex justify-end relative  top-10 sm:right-2">
       <IconButton onClick={handleClick}>
-        <MoreVertIcon className=' text-white' />
+        <MoreVertIcon className=" text-white" />
       </IconButton>
       <Menu
         anchorEl={anchorEl}
@@ -34,12 +33,15 @@ export default function CardModal() {
         onClick={handleClose}
       >
         <MenuItem>
-            <ListItemIcon>
-                
-          <Button >
-            Editar
-            </Button>
-            </ListItemIcon>
+          <ListItemIcon>
+            <div className="flex flex-col gap-2">
+              <Button>Já zerei</Button>
+              <Button>Quero zerar</Button>
+              <Button>Estou jogando</Button>
+              <Button>Desisti de zerar/jogar</Button>
+              <Button>Remover jogo</Button>
+            </div>
+          </ListItemIcon>
         </MenuItem>
       </Menu>
     </div>
